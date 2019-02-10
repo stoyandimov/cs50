dba59ff134ecd64914a5e2a37726eac3d8f3fa12
Stoyan Dimov <stoyandimov@hotmail.com>
2019-02-10 16:04:23 +0000

    Solve week4's problem set (83 out 93 on board)

--- pset4/speller-trie/dictionary.c
+++ pset4/speller-trie/dictionary.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 #include "dictionary.h"
 
@@ -20,9 +22,23 @@ node;
 // Represents a trie
 node *root;
 
+int wordCnt = 0;
+
 // Loads dictionary into memory, returning true if successful else false
 bool load(const char *dictionary)
 {
+    // Open dictionary
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        unload();
+        return false;
+    }
+
+    // Buffer for a word
+    char word[LENGTH + 1];
+
+
     // Initialize trie
     root = malloc(sizeof(node));
     if (root == NULL)
@@ -35,21 +51,26 @@ bool load(const char *dictionary)
         root->children[i] = NULL;
     }
 
-    // Open dictionary
-    FILE *file = fopen(dictionary, "r");
-    if (file == NULL)
-    {
-        unload();
-        return false;
-    }
-
-    // Buffer for a word
-    char word[LENGTH + 1];
-
     // Insert words into trie
     while (fscanf(file, "%s", word) != EOF)
     {
-        // TODO
+        wordCnt++;
+        node *n = root;
+        for (int i = 0; word[i] != '\0'; i++)
+        {
+            int c = word[i] == '\'' ? 26 : tolower(word[i]) - 97;
+            if (n->children[c] == NULL)
+            {
+                n->children[c] = malloc(sizeof(node));
+                for (int ii = 0; ii < N; ii++)
+                {
+                    n->children[c]->children[ii] = NULL;
+                }
+                n->children[c]->is_word = false;
+            }
+            n = n->children[c];
+        }
+        n->is_word = true;
     }
 
     // Close dictionary
@@ -62,20 +83,40 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    return wordCnt;
 }
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    // TODO
-    return false;
+    node *n = root;
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        int c = word[i] == '\'' ? 26 : tolower(word[i]) - 97;
+        if (n->children[c] == NULL)
+        {
+            return false;
+        }
+        n = n->children[c];
+    }
+    return n->is_word;
+}
+
+void clear(node *n)
+{
+    for (int i = 0; i < N; i++)
+    {
+        if (n->children[i] != NULL)
+        {
+            clear(n->children[i]);
+        }
+    }
+    free(n);
 }
 
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    // TODO
-    return false;
+    clear(root);
+    return true;
 }
